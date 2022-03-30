a2c98926f635f790d4113781f3b7e3e0f26ec3dd
yh <1921620519@qq.com>
2022-03-30 16:09:16 +0800

    1

---
+++ app/src/main/java/com/example/zclass/online/Dialog/Dialog_upUser.java
@@ -0,0 +1,138 @@
+package com.example.zclass.online.Dialog;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.zclass.R;
+import com.example.zclass.online.MyInfoActivity;
+import com.example.zclass.online.service.HttpClientUtils;
+
+import java.util.HashMap;
+
+public class Dialog_upUser extends Dialog implements View.OnClickListener{
+    String url_login="http://192.168.0.106:8080/demo_war/LoginServlet";
+    private String title;
+    private String text;
+    private String submit;
+    private TextView msubmit;
+    private String KEY;
+    private TextView mTvtext;
+    private IonsaveListener saveListener;
+    private HashMap<String, String> stringHashMap;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubmit() {
+        return submit;
+    }
+
+    public void setSubmit(String submit) {
+        this.submit = submit;
+    }
+
+    public String getText() {
+        mTvtext=findViewById(R.id.upuser_info);
+        return mTvtext.getText().toString();
+    }
+    public String getKEY() {
+        return KEY;
+    }
+
+    public void setKEY(String KEY) {
+        this.KEY = KEY;
+    }
+
+    public Dialog_upUser setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public TextView getMsubmit() {
+        return msubmit;
+    }
+
+
+    public Dialog_upUser(@NonNull Context context, int themeResId) {
+        super(context,themeResId);
+    }
+
+    public Dialog_upUser setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Dialog_upUser setsubmit(HashMap<String, String> stringHashMap, Dialog_upUser.IonsaveListener Listener) {
+        this.saveListener=Listener;
+        this.stringHashMap =stringHashMap;
+        return this;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_upuser);
+        /*
+         * 将对话框的大小按屏幕大小的百分比设置
+         */
+        WindowManager m = getWindow().getWindowManager();
+        Display d = m.getDefaultDisplay(); // 获取屏幕宽、高用
+        WindowManager.LayoutParams p = getWindow().getAttributes(); // 获取对话框当前的参数值
+        Point size =new Point();
+        d.getSize(size);
+        p.width =(int)(size.x *0.8);//设置dialog的宽度为当前手机屏幕的宽度*0.8
+        getWindow().setAttributes(p);
+
+        mTvtext =findViewById(R.id.upuser_info);
+
+        msubmit =findViewById(R.id.upuser_save);
+
+
+        if(!TextUtils.isEmpty(text)){
+            mTvtext.setText(text);
+        }
+
+        msubmit.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.upuser_save:
+                stringHashMap.put(KEY,getText());
+                saveListener.submit();
+                HttpClientUtils.post(url_login, HttpClientUtils.maptostr(stringHashMap), new HttpClientUtils.OnRequestCallBack() {
+                    @Override
+                    public void onSuccess(String json) {
+                        saveListener.success(json);
+                    }
+                    @Override
+                    public void onError(String errorMsg) {
+                        saveListener.error(errorMsg);
+                    }
+                });
+                break;
+        }
+    }
+
+    public interface IonsaveListener{
+        void submit();
+        void success(String json);
+        void error(String error);
+    }
+}
--- app/src/main/java/com/example/zclass/online/MyInfoActivity.java
+++ app/src/main/java/com/example/zclass/online/MyInfoActivity.java
@@ -1,5 +1,6 @@
 package com.example.zclass.online;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +16,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.zclass.MainActivity;
 import com.example.zclass.R;
+import com.example.zclass.online.Dao.User;
+import com.example.zclass.online.Dialog.Dialog_upUser;
+import com.example.zclass.online.Dialog.LoadingDialog;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.HashMap;
+
 public class MyInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
     private ImageView mTVuic;
     private TextView mTVuichang;
     private TextView mTVuid;
@@ -26,6 +34,7 @@ public class MyInfoActivity extends AppCompatActivity implements View.OnClickLis
     private TextView mTVuname;
     private TextView mTVuprofess;
     private TextView mTVuschool;
+    private TextView mTVuphone;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.myinfo);
@@ -62,19 +71,28 @@ public class MyInfoActivity extends AppCompatActivity implements View.OnClickLis
         mTVuid.setText(MainActivity.user_info.getUserid());
         mTVusex=findViewById(R.id.info_sex);
         setpic(mTVusex);
+        mTVusex.setOnClickListener(this);
         mTVusex.setText(MainActivity.user_info.getSex());
         mTVupassw=findViewById(R.id.info_passw);
         setpic(mTVupassw);
+        mTVupassw.setOnClickListener(this);
         mTVupassw.setText(MainActivity.user_info.getPassword());
         mTVuname=findViewById(R.id.info_username);
         setpic(mTVuname);
+        mTVuname.setOnClickListener(this);
         mTVuname.setText(MainActivity.user_info.getUsername());
         mTVuprofess=findViewById(R.id.info_profess);
         setpic(mTVuprofess);
+        mTVuprofess.setOnClickListener(this);
         mTVuprofess.setText(MainActivity.user_info.getProfess());
         mTVuschool=findViewById(R.id.info_school);
         mTVuschool.setText(MainActivity.user_info.getSchool());
         setpic(mTVuschool);
+        mTVuschool.setOnClickListener(this);
+        mTVuphone=findViewById(R.id.info_phone);
+        mTVuphone.setText(MainActivity.user_info.getPhonenumber());
+        setpic(mTVuphone);
+        mTVuphone.setOnClickListener(this);
         super.onCreate(savedInstanceState);
     }
     public void setpic(TextView mETpassword){
@@ -96,14 +114,112 @@ public class MyInfoActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.info_userid:
                 break;
             case R.id.info_sex:
+                HashMap<String, String> stringHashMap2=new HashMap<String,String>();
+                stringHashMap2.put(User.WAY,"upsex");
+                toast(User.SEX,mTVusex.getText().toString(),stringHashMap2);
                 break;
             case R.id.info_passw:
+                HashMap<String, String> stringHashMap3=new HashMap<String,String>();
+                stringHashMap3.put(User.WAY,"uppass");
+                toast(User.PASSWORD,mTVupassw.getText().toString(),stringHashMap3);
                 break;
             case R.id.info_username:
+                HashMap<String, String> stringHashMap4=new HashMap<String,String>();
+                stringHashMap4.put(User.WAY,"upname");
+                toast(User.USERNAME,mTVuname.getText().toString(),stringHashMap4);
                 break;
             case R.id.info_school:
+                HashMap<String, String> stringHashMap5=new HashMap<String,String>();
+                stringHashMap5.put(User.WAY,"upschool");
+                toast(User.SCHOOL,mTVuschool.getText().toString(),stringHashMap5);
+                break;
+            case R.id.info_phone:
+                HashMap<String, String> stringHashMap6=new HashMap<String,String>();
+                stringHashMap6.put(User.WAY,"upphone");
+                toast(User.PHONENUMBER,mTVuphone.getText().toString(),stringHashMap6);
+                break;
+            case R.id.info_profess:
+                HashMap<String, String> stringHashMap7=new HashMap<String,String>();
+                stringHashMap7.put(User.WAY,"upprof");
+                toast(User.PROFESS,mTVuprofess.getText().toString(),stringHashMap7);
                 break;
 
         }
+    }
+    public void toast(String KEY, String text, HashMap<String,String> stringHashMap){
+        HashMap<String, String> stringHashMap2=new HashMap<String,String>();
+        stringHashMap2.put(User.USERID, MainActivity.user_info.getUserid());
+        stringHashMap2.put(User.METHOD,"login");
+        stringHashMap2.putAll(stringHashMap);
+
+        Dialog dialog = LoadingDialog.createLoadingDialog(MyInfoActivity.this);
+
+        Dialog_upUser Dialod_upsex = new Dialog_upUser(MyInfoActivity.this,R.style.MyDialog);
+        Dialod_upsex.setKEY(KEY);
+        Dialod_upsex.setText(text).setsubmit(stringHashMap2, new Dialog_upUser.IonsaveListener() {
+            @Override
+            public void submit() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.show();
+                    }
+                });
+            }
+            @Override
+            public void success(String json) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.hide();
+                        if("Ok".equals(json)){
+                            Toast.makeText(getApplicationContext(), "更新成功!",
+                                    Toast.LENGTH_SHORT).show();
+                            switch (stringHashMap.get("way")){
+                                case "upsex":
+                                    MainActivity.user_info.setSex(Dialod_upsex.getText());
+                                    mTVusex.setText(Dialod_upsex.getText());
+                                    break;
+                                case "uppass":
+                                    MainActivity.user_info.setPassword(Dialod_upsex.getText());
+                                    mTVupassw.setText(Dialod_upsex.getText());
+                                    break;
+                                case "upname":
+                                    MainActivity.user_info.setUsername(Dialod_upsex.getText());
+                                    mTVuname.setText(Dialod_upsex.getText());
+                                    break;
+                                case "upschool":
+                                    MainActivity.user_info.setSchool(Dialod_upsex.getText());
+                                    mTVuschool.setText(Dialod_upsex.getText());
+                                    break;
+                                case "upphone":
+                                    MainActivity.user_info.setPhonenumber(Dialod_upsex.getText());
+                                    mTVuphone.setText(Dialod_upsex.getText());
+                                    break;
+                                case "upprof":
+                                    MainActivity.user_info.setProfess(Dialod_upsex.getText());
+                                    mTVuprofess.setText(Dialod_upsex.getText());
+                                    break;
+                            }
+                            Dialod_upsex.cancel();
+                        }else {
+                            Toast.makeText(getApplicationContext(), "更新失败!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+            @Override
+            public void error(String error) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        dialog.hide();
+                        Toast.makeText(getApplicationContext(), "网络崩溃!/n"+error,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).show();
     }
 }
